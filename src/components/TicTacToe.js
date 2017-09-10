3adc720e9193516dddf55ec13234d86cf3d85e6d
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-09-10 12:25:32 +0200

    working game

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -22,6 +22,10 @@ const Cell = styled.div`
   height: calc(100% / 3 - 2px);
   float: left;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
   border-right: ${props => [0, 1, 3, 4, 6, 7].includes(props.id) 
     ? '2px solid black' : 'none'};
   border-bottom: ${props => [0, 1, 2, 3, 4, 5].includes(props.id)
@@ -35,22 +39,76 @@ class TicTacToe extends Component {
     this.state = {
       board: new Array(9).fill(' '),
       playerTurn: props.firstTurn === 'player',
-      winner: null
+      gameIsPlaying: true,
+      gameResult: null
     };
   }
 
   componentDidMount() {
     let { firstTurn } = this.props;
-    console.log('comp?', firstTurn);
     if (firstTurn === 'computer') {
+    // make first computer's move
       setTimeout(() => {
         let { board } = this.state;
         const { computerMark, playerMark } = this.props;
         let computerMoveIndex = aiMove(board, computerMark, playerMark);
-        console.log('index', computerMoveIndex);
         board.splice(computerMoveIndex, 1, computerMark);
         this.setState({ board, playerTurn: true });
       }, 500);
+    }
+  }
+
+  playerMove = index => {
+    const { playerMark, computerMark } = this.props;
+    let { board, gameIsPlaying, playerTurn } = this.state;
+
+    if (gameIsPlaying && playerTurn && isEmpty(board, index)) {
+      board.splice(index, 1, playerMark);
+      this.setState({ board });
+    
+      if (isWinner(board, playerMark)) {
+        this.setState({ // player won
+          gameIsPlaying: false,
+          gameResult: 'Wygrałeś!!!'
+        });
+      } else { 
+        if (isBoardFull(board)) { // draw
+          this.setState({
+            gameIsPlaying: false,
+            gameResult: 'Remis'
+          });
+        } else { // computer move
+          this.setState({ playerTurn: false });
+          setTimeout(() => this.computerMove(), 500);
+        }
+      }
+    }
+  }
+
+  computerMove = () => {
+    const { playerMark, computerMark } = this.props;
+    let { board, gameIsPlaying } = this.state;
+
+    if (gameIsPlaying) {
+      let computerMoveIndex = aiMove(board, computerMark, playerMark);
+      board.splice(computerMoveIndex, 1, computerMark);
+      this.setState({ board });
+    
+      if (isWinner(board, computerMark)) { // computer won
+        this.setState({ 
+          gameIsPlaying: false, 
+          gameResult: 'Przegrałeś...'
+        });
+      } else {
+        if (isBoardFull(board)) { // draw
+          this.setState({
+            gameIsPlaying: false,
+            gameResult: 'Remis'
+          });
+        } else { // player move
+          this.setState({ playerTurn: true });
+        }
+      }
     }
   }
 
@@ -61,12 +119,17 @@ class TicTacToe extends Component {
           {
             this.state.board.map((cell, index) => {
               return (
-                <Cell key={index} id={index}>
+                <Cell 
+                  key={index} 
+                  id={index}
+                  onClick={() => this.playerMove(index)}
+                >
                   { cell }
                 </Cell>
               )
             })
           }
+          { this.state.gameResult }
         </GameBoard>
       </div>
     );
